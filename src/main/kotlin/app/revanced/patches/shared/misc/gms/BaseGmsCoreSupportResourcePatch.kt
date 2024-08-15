AmpleReVanced/revanced-patches

commit 36e8707ab93e6edb61e9d5f2c38acb8ba0a58551

feat(Google Photos): Add `GmsCore support` patch (#3414)
Co-authored-by: benjy3gg <benjy3gg@gmail.com>
Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

src/main/kotlin/app/revanced/patches/shared/misc/gms/BaseGmsCoreSupportResourcePatch.kt
@@ -96,27 +96,23 @@ abstract class BaseGmsCoreSupportResourcePatch(
     private fun ResourceContext.patchManifest() {
         val packageName = ChangePackageNamePatch.setOrGetFallbackPackageName(toPackageName)
 
-        val manifest = this.get("AndroidManifest.xml").readText()
-        this.get("AndroidManifest.xml").writeText(
-            manifest.replace(
-                "package=\"$fromPackageName",
-                "package=\"$packageName",
-            ).replace(
-                "android:authorities=\"$fromPackageName",
-                "android:authorities=\"$packageName",
-            ).replace(
-                "$fromPackageName.permission.C2D_MESSAGE",
-                "$packageName.permission.C2D_MESSAGE",
-            ).replace(
-                "$fromPackageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION",
-                "$packageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION",
-            ).replace(
-                "com.google.android.c2dm",
-                "$gmsCoreVendorGroupId.android.c2dm",
-            ).replace(
-                "</queries>",
-                "<package android:name=\"$gmsCoreVendorGroupId.android.gms\"/></queries>",
-            ),
+        val transformations = mapOf(
+            "package=\"$fromPackageName" to "package=\"$packageName",
+            "android:authorities=\"$fromPackageName" to "android:authorities=\"$packageName",
+            "$fromPackageName.permission.C2D_MESSAGE" to "$packageName.permission.C2D_MESSAGE",
+            "$fromPackageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION" to "$packageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION",
+            "com.google.android.c2dm" to "$packageName.android.c2dm",
+            "com.google.android.libraries.photos.api.mars" to "$packageName.android.apps.photos.api.mars",
+            "</queries>" to "<package android:name=\"$gmsCoreVendorGroupId.android.gms\"/></queries>",
+        )
+
+        get("AndroidManifest.xml", false).writeText(
+            transformations.entries.fold(get("AndroidManifest.xml", false).readText()) { acc, (from, to) ->
+                acc.replace(
+                    from,
+                    to
+                )
+            }
         )
     }
 

api/revanced-patches.api
@@ -235,6 +235,18 @@ public final class app/revanced/patches/googlenews/misc/integrations/Integration
 	public static final field INSTANCE Lapp/revanced/patches/googlenews/misc/integrations/IntegrationsPatch;
 }
 
+public final class app/revanced/patches/googlephotos/misc/gms/GmsCoreSupportPatch : app/revanced/patches/shared/misc/gms/BaseGmsCoreSupportPatch {
+	public static final field INSTANCE Lapp/revanced/patches/googlephotos/misc/gms/GmsCoreSupportPatch;
+}
+
+public final class app/revanced/patches/googlephotos/misc/gms/GmsCoreSupportResourcePatch : app/revanced/patches/shared/misc/gms/BaseGmsCoreSupportResourcePatch {
+	public static final field INSTANCE Lapp/revanced/patches/googlephotos/misc/gms/GmsCoreSupportResourcePatch;
+}
+
+public final class app/revanced/patches/googlephotos/misc/integrations/IntegrationsPatch : app/revanced/patches/shared/misc/integrations/BaseIntegrationsPatch {
+	public static final field INSTANCE Lapp/revanced/patches/googlephotos/misc/integrations/IntegrationsPatch;
+}
+
 public final class app/revanced/patches/googlerecorder/restrictions/RemoveDeviceRestrictions : app/revanced/patcher/patch/BytecodePatch {
 	public static final field INSTANCE Lapp/revanced/patches/googlerecorder/restrictions/RemoveDeviceRestrictions;
 	public fun execute (Lapp/revanced/patcher/data/BytecodeContext;)V

src/main/kotlin/app/revanced/patches/all/misc/resources/AddResourcesPatch.kt
@@ -347,7 +347,10 @@ object AddResourcesPatch : ResourcePatch(), MutableMap<Value, MutableSet<BaseRes
                 val targetFile =
                     context.get("res/$value/$resourceFileName.xml").also {
                         it.parentFile?.mkdirs()
-                        it.createNewFile()
+
+                        if(it.createNewFile()) {
+                            it.writeText("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n</resources>")
+                        }
                     }
 
                 context.xmlEditor[targetFile.path].let { editor ->

src/main/kotlin/app/revanced/patches/googlenews/misc/gms/GmsCoreSupportPatch.kt
@@ -4,7 +4,6 @@ import app.revanced.patches.googlenews.misc.gms.Constants.MAGAZINES_PACKAGE_NAME
 import app.revanced.patches.googlenews.misc.gms.Constants.REVANCED_MAGAZINES_PACKAGE_NAME
 import app.revanced.patches.googlenews.misc.gms.GmsCoreSupportResourcePatch.gmsCoreVendorGroupIdOption
 import app.revanced.patches.googlenews.misc.gms.fingerprints.MagazinesActivityOnCreateFingerprint
-import app.revanced.patches.googlenews.misc.gms.fingerprints.ServiceCheckFingerprint
 import app.revanced.patches.googlenews.misc.integrations.IntegrationsPatch
 import app.revanced.patches.shared.misc.gms.BaseGmsCoreSupportPatch
 
@@ -13,14 +12,12 @@ object GmsCoreSupportPatch : BaseGmsCoreSupportPatch(
     fromPackageName = MAGAZINES_PACKAGE_NAME,
     toPackageName = REVANCED_MAGAZINES_PACKAGE_NAME,
     primeMethodFingerprint = null,
-    earlyReturnFingerprints = setOf(ServiceCheckFingerprint),
     mainActivityOnCreateFingerprint = MagazinesActivityOnCreateFingerprint,
     integrationsPatchDependency = IntegrationsPatch::class,
     gmsCoreSupportResourcePatch = GmsCoreSupportResourcePatch,
     // Remove version constraint,
     // once https://github.com/ReVanced/revanced-patches/pull/3111#issuecomment-2240877277 is resolved.
     compatiblePackages = setOf(CompatiblePackage(MAGAZINES_PACKAGE_NAME, setOf("5.108.0.644447823"))),
-    fingerprints = setOf(ServiceCheckFingerprint),
 ) {
     override val gmsCoreVendorGroupId by gmsCoreVendorGroupIdOption
 }

src/main/kotlin/app/revanced/patches/googlephotos/misc/gms/Constants.kt
@@ -1,0 +1,6 @@
+package app.revanced.patches.googlephotos.misc.gms
+
+internal object Constants {
+    const val PHOTOS_PACKAGE_NAME = "com.google.android.apps.photos"
+    const val REVANCED_PHOTOS_PACKAGE_NAME = "app.revanced.android.photos"
+}

src/main/kotlin/app/revanced/patches/googlephotos/misc/gms/GmsCoreSupportPatch.kt
@@ -1,0 +1,21 @@
+package app.revanced.patches.googlephotos.misc.gms
+
+import app.revanced.patches.googlephotos.misc.gms.Constants.PHOTOS_PACKAGE_NAME
+import app.revanced.patches.googlephotos.misc.gms.Constants.REVANCED_PHOTOS_PACKAGE_NAME
+import app.revanced.patches.googlephotos.misc.gms.GmsCoreSupportResourcePatch.gmsCoreVendorGroupIdOption
+import app.revanced.patches.googlephotos.misc.gms.fingerprints.PhotosActivityOnCreateFingerprint
+import app.revanced.patches.googlephotos.misc.integrations.IntegrationsPatch
+import app.revanced.patches.shared.misc.gms.BaseGmsCoreSupportPatch
+
+@Suppress("unused")
+object GmsCoreSupportPatch : BaseGmsCoreSupportPatch(
+    fromPackageName = PHOTOS_PACKAGE_NAME,
+    toPackageName = REVANCED_PHOTOS_PACKAGE_NAME,
+    primeMethodFingerprint = null,
+    mainActivityOnCreateFingerprint = PhotosActivityOnCreateFingerprint,
+    integrationsPatchDependency = IntegrationsPatch::class,
+    gmsCoreSupportResourcePatch = GmsCoreSupportResourcePatch,
+    compatiblePackages = setOf(CompatiblePackage(PHOTOS_PACKAGE_NAME)),
+) {
+    override val gmsCoreVendorGroupId by gmsCoreVendorGroupIdOption
+}

src/main/kotlin/app/revanced/patches/googlephotos/misc/gms/GmsCoreSupportResourcePatch.kt
@@ -1,0 +1,11 @@
+package app.revanced.patches.googlephotos.misc.gms
+
+import app.revanced.patches.googlephotos.misc.gms.Constants.PHOTOS_PACKAGE_NAME
+import app.revanced.patches.googlephotos.misc.gms.Constants.REVANCED_PHOTOS_PACKAGE_NAME
+import app.revanced.patches.shared.misc.gms.BaseGmsCoreSupportResourcePatch
+
+object GmsCoreSupportResourcePatch : BaseGmsCoreSupportResourcePatch(
+    fromPackageName = PHOTOS_PACKAGE_NAME,
+    toPackageName = REVANCED_PHOTOS_PACKAGE_NAME,
+    spoofedPackageSignature = "24bb24c05e47e0aefa68a58a766179d9b613a600",
+)

src/main/kotlin/app/revanced/patches/googlephotos/misc/gms/fingerprints/PhotosActivityOnCreateFingerprint.kt
@@ -1,0 +1,9 @@
+package app.revanced.patches.googlephotos.misc.gms.fingerprints
+
+import app.revanced.patcher.fingerprint.MethodFingerprint
+
+internal object PhotosActivityOnCreateFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, classDef ->
+        methodDef.name == "onCreate" && classDef.endsWith("/HomeActivity;")
+    },
+)

commit 17be7d118affe0f2de8154c8e9cc7dd13383443c

fix(YouTube - GmsCore support): Fix notifications not working by using the correct permissions
Regression had been introduced in 36e8707ab93e6edb61e9d5f2c38acb8ba0a58551. This commit reverts this.

src/main/kotlin/app/revanced/patches/shared/misc/gms/BaseGmsCoreSupportResourcePatch.kt
@@ -101,8 +101,8 @@ abstract class BaseGmsCoreSupportResourcePatch(
             "android:authorities=\"$fromPackageName" to "android:authorities=\"$packageName",
             "$fromPackageName.permission.C2D_MESSAGE" to "$packageName.permission.C2D_MESSAGE",
             "$fromPackageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION" to "$packageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION",
-            "com.google.android.c2dm" to "$packageName.android.c2dm",
-            "com.google.android.libraries.photos.api.mars" to "$packageName.android.apps.photos.api.mars",
+            "com.google.android.c2dm" to "$gmsCoreVendorGroupId.android.c2dm",
+            "com.google.android.libraries.photos.api.mars" to "$gmsCoreVendorGroupId.android.apps.photos.api.mars",
             "</queries>" to "<package android:name=\"$gmsCoreVendorGroupId.android.gms\"/></queries>",
         )
 
@@ -110,9 +110,9 @@ abstract class BaseGmsCoreSupportResourcePatch(
             transformations.entries.fold(get("AndroidManifest.xml", false).readText()) { acc, (from, to) ->
                 acc.replace(
                     from,
-                    to
+                    to,
                 )
-            }
+            },
         )
     }
 