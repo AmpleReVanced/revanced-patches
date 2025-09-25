658a8f2707c49b934b84df0219b5c31cbf2ead5e
naijun0403 <naijun0403@gmail.com>
2025-09-25 22:51:32 +0900

    feat(kakaotalk): update mention handling for compatibility with KakaoTalk 25.8.1

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/chatroom/BypassInputMentionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/chatroom/BypassInputMentionPatch.kt
@@ -1,25 +1,9 @@
 package app.revanced.patches.kakaotalk.chatroom
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.instructions
-import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patches.kakaotalk.chatroom.fingerprints.checkMentionableFingerprint
-import app.revanced.patches.kakaotalk.chatroom.fingerprints.isMultiChatFingerprint
-import app.revanced.patches.kakaotalk.chatroom.fingerprints.limitMentionToNonMultiChatFingerprint
-import app.revanced.patches.kakaotalk.chatroom.fingerprints.mentionComponentClassFingerprint
-import app.revanced.util.getReference
-import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction11n
-import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction11x
-import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction21c
-import com.android.tools.smali.dexlib2.iface.reference.MethodReference
-import com.android.tools.smali.dexlib2.iface.reference.TypeReference
+import app.revanced.patches.kakaotalk.chatroom.fingerprints.mentionComponentIsMultiChatFingerprint
 
-/**
- * TODO: Not working at 25.8.1, needs rework
- */
 @Suppress("unused")
 val bypassInputMentionPatch = bytecodePatch(
     name = "Bypass input mention limit in non-multichat",
@@ -28,31 +12,12 @@ val bypassInputMentionPatch = bytecodePatch(
     compatibleWith("com.kakao.talk"("25.8.1"))
 
     execute {
-        val mentionComponent = mentionComponentClassFingerprint.method.definingClass
-        limitMentionToNonMultiChatFingerprint.method.instructions
-            .indexOfFirst { it.opcode == Opcode.NEW_INSTANCE && (it as Instruction21c).getReference<TypeReference>()?.type == mentionComponent }
-            .takeIf { it >= 0 }
-            ?.let { index ->
-                limitMentionToNonMultiChatFingerprint.method.removeInstruction(index - 1)
-            }
-
-        val isMultiChatMethod = isMultiChatFingerprint.method
-
-        checkMentionableFingerprint.method.instructions
-            .indexOfFirst { it.opcode == Opcode.INVOKE_STATIC
-                    && it.getReference<MethodReference>()?.definingClass == isMultiChatMethod.definingClass
-                    && it.getReference<MethodReference>()?.name == isMultiChatMethod.name }
-            .takeIf { it >= 0 }
-            ?.let { index ->
-                val register = (checkMentionableFingerprint.method.getInstruction(index + 1) as Instruction11x).registerA
-                checkMentionableFingerprint.method.addInstruction(
-                    index + 2,
-                    BuilderInstruction11n(
-                        Opcode.CONST_4,
-                        register,
-                        0x1
-                    )
-                )
-            }
+        mentionComponentIsMultiChatFingerprint.method.replaceInstructions(
+            0,
+            """
+                const/4 v0, 0x1
+                return v0
+            """.trimIndent()
+        )
     }
 }
