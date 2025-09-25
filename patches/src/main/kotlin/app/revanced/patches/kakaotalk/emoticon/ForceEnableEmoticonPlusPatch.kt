4a7013c299b9b74cefc6c05fe3c6fa2858c173a9
naijun0403 <naijun0403@gmail.com>
2025-09-26 00:00:47 +0900

    feat(kakaotalk): refactor emoticon plus fingerprint and patch for improved functionality

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/emoticon/ForceEnableEmoticonPlusPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/emoticon/ForceEnableEmoticonPlusPatch.kt
@@ -4,14 +4,10 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.kakaotalk.emoticon.fingerprints.isEnableEmoticonPlusFingerprint
 
-/**
- * Not working at 25.8.1, needs rework
- */
 @Suppress("unused")
 val forceEnableEmoticonPlusPatch = bytecodePatch(
     name = "Force enable emoticon plus feature",
     description = "Force enable emoticon plus feature (Unpurchased emoticon can be sent once per day)",
-    use = false
 ) {
     compatibleWith("com.kakao.talk"("25.8.1"))
 
