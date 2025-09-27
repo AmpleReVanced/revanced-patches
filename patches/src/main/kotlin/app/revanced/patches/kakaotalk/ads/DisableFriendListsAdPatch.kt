7a636e8e2e21d99d3d7da25cec2011133287f62f
naijun0403 <naijun0403@gmail.com>
2025-09-27 23:30:36 +0900

    feat(kakaotalk): add patch to disable Friend Lists ad with corresponding fingerprint

---
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/ads/DisableFriendListsAdPatch.kt
@@ -0,0 +1,25 @@
+package app.revanced.patches.kakaotalk.ads
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.kakaotalk.ads.fingerprints.checkDisableFriendListsAdFingerprint
+
+@Suppress("unused")
+val disableFriendListsAdPatch = bytecodePatch(
+    name = "Disable Friend Lists ad",
+    description = "Disables the Friend Lists ad in KakaoTalk.",
+) {
+    compatibleWith("com.kakao.talk"("25.8.2"))
+
+    execute {
+        val checkDisableFriendListsAdMethod = checkDisableFriendListsAdFingerprint.method
+
+        checkDisableFriendListsAdMethod.addInstructions(
+            0,
+            """
+                const/4 v0, 0x1
+                return v0
+            """.trimIndent()
+        )
+    }
+}
