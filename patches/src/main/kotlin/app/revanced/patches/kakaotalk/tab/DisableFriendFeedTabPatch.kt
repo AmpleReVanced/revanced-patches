bf6906b37ea380eb615f3a19308f1a684b4f52d8
naijun0403 <naijun0403@gmail.com>
2025-09-27 19:47:14 +0900

    feat(kakaotalk): add patch to disable Friend Feed tab and corresponding fingerprint

---
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/tab/DisableFriendFeedTabPatch.kt
@@ -0,0 +1,24 @@
+package app.revanced.patches.kakaotalk.tab
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.instructions
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.kakaotalk.tab.fingerprints.mainTabConfigFingerprint
+
+@Suppress("unused")
+val disableFriendFeedTabPatch = bytecodePatch(
+    name = "Disable Friend Feed tab",
+    description = "Disables the Friend Feed tab in KakaoTalk.",
+) {
+    compatibleWith("com.kakao.talk"("25.8.2"))
+
+    execute {
+        mainTabConfigFingerprint.method.addInstructions(
+            mainTabConfigFingerprint.method.instructions.size - 1,
+            """
+                const/4 p1, 0x0
+                iput-boolean p1, p0, ${mainTabConfigFingerprint.method.definingClass}->a:Z
+            """.trimIndent()
+        )
+    }
+}
