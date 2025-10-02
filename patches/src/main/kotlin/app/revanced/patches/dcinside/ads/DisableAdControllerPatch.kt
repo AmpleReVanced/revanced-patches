daab786c4f249f2d5be1dd6660fe91c177f17650
naijun0403 <naijun0403@gmail.com>
2025-10-02 14:34:50 +0900

    feat(dcinisde): update compatibility for dcinside to version 5.1.5

--- patches/src/main/kotlin/app/revanced/patches/dcinside/ads/DisableAdControllerPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/dcinside/ads/DisableAdControllerPatch.kt
@@ -9,7 +9,7 @@ val disableAdControllerPatch = bytecodePatch(
     name = "Disable ad controller",
     description = "Disables the ad controller that manages ads in the app.",
 ) {
-    compatibleWith("com.dcinside.app.android"("5.1.4"))
+    compatibleWith("com.dcinside.app.android"("5.1.5"))
 
     execute {
         disableAdControllerFingerprint.method.addInstructions(
