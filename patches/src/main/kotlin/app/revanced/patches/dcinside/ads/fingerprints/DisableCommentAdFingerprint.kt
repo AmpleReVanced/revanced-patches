4943e005e3cbfa788cd00f210b977fe9ef93bb49
naijun0403 <naijun0403@gmail.com>
2025-10-09 00:38:39 +0900

    feat(dcinside): add patch for PostReadReplyTopAdView to disable ad visibility

--- patches/src/main/kotlin/app/revanced/patches/dcinside/ads/fingerprints/DisableCommentAdFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/dcinside/ads/fingerprints/DisableCommentAdFingerprint.kt
@@ -7,5 +7,12 @@ internal val postReadReplyAdViewFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.CONSTRUCTOR)
     parameters("Landroid/content/Context;", "Landroid/util/AttributeSet;", "I")
     returns("V")
-    custom { method, classDef -> classDef.toString() == "Lcom/dcinside/app/view/PostReadReplyAdView;" }
+    custom { method, classDef -> classDef.type == "Lcom/dcinside/app/view/PostReadReplyAdView;" }
+}
+
+internal val postReadReplyTopAdViewFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.CONSTRUCTOR)
+    parameters("Landroid/content/Context;", "Landroid/util/AttributeSet;", "I")
+    returns("V")
+    custom { method, classDef -> classDef.type == "Lcom/dcinside/app/view/PostReadReplyTopAdView;" }
 }
