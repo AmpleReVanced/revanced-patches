e52e87b37dcf6fb252da21d693a1007c0abc33d5
naijun0403 <naijun0403@gmail.com>
2025-10-26 17:02:42 +0900

    feat(kakaotalk): enhance spoofApkChecksumsPatch to load APK checksums from resource

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/integrity/SpoofApkChecksumsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/integrity/SpoofApkChecksumsPatch.kt
@@ -1,6 +1,13 @@
 package app.revanced.patches.kakaotalk.integrity
 
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.instructions
+import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patches.kakaotalk.integrity.fingerprints.getApkChecksumsFingerprint
+import app.revanced.util.getReference
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 
 @Suppress("unused")
 val spoofApkChecksumsPatch = bytecodePatch(
@@ -10,6 +17,25 @@ val spoofApkChecksumsPatch = bytecodePatch(
     compatibleWith("com.kakao.talk"("25.9.0"))
 
     execute {
+        val getApkChecksumsMethod = getApkChecksumsFingerprint.method
+        val sputObject = getApkChecksumsMethod.instructions.first {
+            it.opcode == Opcode.SPUT_OBJECT
+        }
 
+        val apkChecksums =
+            ::javaClass.javaClass.classLoader.getResourceAsStream("kakaotalk/apkChecksum")?.bufferedReader()?.readText()
+                ?: throw PatchException("Failed to load original signature resource.")
+
+        val variable = sputObject.getReference<FieldReference>()?.name ?: throw PatchException("Failed to get apk checksum field name.")
+
+        getApkChecksumsMethod.addInstructions(
+            sputObject.location.index + 1,
+            """
+                const-string v0, "$apkChecksums"
+                invoke-virtual {v0}, Ljava/lang/String;->toCharArray()[C
+                move-result-object v0
+                sput-object v0, Lcom/kakao/talk/util/AbuseDetectUtil;->$variable:[C
+            """.trimIndent()
+        )
     }
 }
