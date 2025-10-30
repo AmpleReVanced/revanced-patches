7669b9a775400ce4bb622b2c8930143a81c00a6b
naijun0403 <naijun0403@gmail.com>
2025-10-30 22:04:34 +0900

    fix(kakaotalk): update getResourceId to use fixed package name in ResourceHelper

--- extensions/kakaotalk/src/main/java/app/revanced/extension/kakaotalk/helper/ResourceHelper.java
+++ extensions/kakaotalk/src/main/java/app/revanced/extension/kakaotalk/helper/ResourceHelper.java
@@ -5,7 +5,8 @@ import app.revanced.extension.shared.Utils;
 public class ResourceHelper {
 
     public static int getResourceId(String defType, String name) {
-        return Utils.getContext().getResources().getIdentifier(name, defType, Utils.getContext().getPackageName());
+        // TODO: To prevent issues when cloning, you must change the package name in revanced-patcher along with the one in arsc.
+        return Utils.getContext().getResources().getIdentifier(name, defType, "com.kakao.talk");
     }
 
 }
