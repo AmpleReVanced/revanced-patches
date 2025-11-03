b48a4b25504e5f7fbb7015b1c429952569e3dc6a
naijun0403 <naijun0403@gmail.com>
2025-11-03 15:53:21 +0900

    fix: update ResourceHelper to use dynamic package name for resource identification

--- extensions/kakaotalk/src/main/java/app/revanced/extension/kakaotalk/helper/ResourceHelper.java
+++ extensions/kakaotalk/src/main/java/app/revanced/extension/kakaotalk/helper/ResourceHelper.java
@@ -5,8 +5,7 @@ import app.revanced.extension.shared.Utils;
 public class ResourceHelper {
 
     public static int getResourceId(String defType, String name) {
-        // TODO: To prevent issues when cloning, you must change the package name in revanced-patcher along with the one in arsc.
-        return Utils.getContext().getResources().getIdentifier(name, defType, "com.kakao.talk");
+        return Utils.getContext().getResources().getIdentifier(name, defType, Utils.getContext().getPackageName());
     }
 
 }
