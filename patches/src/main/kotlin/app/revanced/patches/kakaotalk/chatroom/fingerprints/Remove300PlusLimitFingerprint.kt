bab28ccd36291c41ee4283e823f59877a377958c
naijun0403 <naijun0403@gmail.com>
2025-11-10 21:33:27 +0900

    feat(kakaotalk): Refactor chatroom patch to remove 300+ unread limit and add getUnreadCount fingerprint

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/chatroom/fingerprints/Remove300PlusLimitFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/chatroom/fingerprints/Remove300PlusLimitFingerprint.kt
@@ -2,8 +2,8 @@ package app.revanced.patches.kakaotalk.chatroom.fingerprints
 
 import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
 
-@Suppress("unused")
 internal val limit300PlusBaseChatRoomFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
     returns("V")
@@ -12,11 +12,26 @@ internal val limit300PlusBaseChatRoomFingerprint = fingerprint {
     custom { method, classDef -> classDef.sourceFile == "BaseChatRoomItem.kt" }
 }
 
-@Suppress("unused")
 internal val limit300PlusOpenChatRoomFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC)
     returns("V")
     parameters()
     strings("300+")
     custom { method, classDef -> classDef.sourceFile == "OpenLinkChatsItem.kt" }
+}
+
+internal val getUnreadCountFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL, AccessFlags.DECLARED_SYNCHRONIZED)
+    returns("I")
+    parameters()
+    opcodes(
+        Opcode.MONITOR_ENTER,
+        Opcode.IGET,
+        Opcode.IGET_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT,
+        Opcode.ADD_INT_2ADDR,
+        Opcode.CONST_16
+    )
+    custom { method, classDef -> classDef.sourceFile == "UnreadCountInfo.kt" }
 }
