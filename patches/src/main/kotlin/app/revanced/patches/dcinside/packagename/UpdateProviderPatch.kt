0b25b0a8961ffb96704d8505d45b8a7e3c35453c
naijun0403 <naijun0403@gmail.com>
2026-03-31 00:36:11 +0900

    fix(dcinside): fixed can't install clone variant

--- patches/src/main/kotlin/app/revanced/patches/dcinside/packagename/UpdateProviderPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/dcinside/packagename/UpdateProviderPatch.kt
@@ -1,10 +1,11 @@
 package app.revanced.patches.dcinside.packagename
 
 import app.morphe.patcher.patch.resourcePatch
+import app.morphe.util.getNode
 import app.revanced.patches.all.misc.packagename.changePackageNamePatch
 import app.revanced.patches.all.misc.packagename.packageNameOption
-import app.morphe.util.getNode
 import app.revanced.patches.dcinside.shared.Constants.COMPATIBILITY_DC_INSIDE
+import org.w3c.dom.Element
 
 @Suppress("unused")
 val updateProviderPatch = resourcePatch(
@@ -16,26 +17,42 @@ val updateProviderPatch = resourcePatch(
     dependsOn(changePackageNamePatch)
 
     execute {
+        val originalPackageName = "com.dcinside.app.android"
         val replacementPackageName = packageNameOption.value
         val newPackageName = if (replacementPackageName != packageNameOption.default) {
             replacementPackageName!!
         } else {
-            "com.dcinside.app.android.revanced"
+            "$originalPackageName.revanced"
         }
 
         document("res/values/strings.xml").use { document ->
-            val resources = document.getNode("resources") as org.w3c.dom.Element
+            val resources = document.getNode("resources") as Element
             val stringNodes = resources.getElementsByTagName("string")
 
             for (i in 0 until stringNodes.length) {
-                val stringElement = stringNodes.item(i) as org.w3c.dom.Element
+                val stringElement = stringNodes.item(i) as Element
                 val textContent = stringElement.textContent?.trim()
 
-                if (textContent != null && textContent.startsWith("com.dcinside.app.android")) {
-                    val updatedValue = textContent.replace("com.dcinside.app.android", newPackageName)
+                if (textContent != null && textContent.startsWith(originalPackageName)) {
+                    val updatedValue = textContent.replace(originalPackageName, newPackageName)
                     stringElement.textContent = updatedValue
+                }
+            }
+        }
 
-                    val stringName = stringElement.getAttribute("name")
+        document("AndroidManifest.xml").use { document ->
+            val manifest = document.getNode("manifest") as Element
+            val providerNodes = manifest.getElementsByTagName("provider")
+
+            for (i in 0 until providerNodes.length) {
+                val provider = providerNodes.item(i) as Element
+                val authorities = provider.getAttribute("android:authorities")
+
+                if (authorities.startsWith(originalPackageName)) {
+                    provider.setAttribute(
+                        "android:authorities",
+                        authorities.replace(originalPackageName, newPackageName),
+                    )
                 }
             }
         }
