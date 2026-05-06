b994f70e228f9b2f5d4db78cdecc37cca9176df3
naijun0403 <naijun0403@gmail.com>
2026-05-06 14:13:13 +0900

    fix(kakaotalk): restore force emoticon plus activation
    Fixes #40

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/emoticon/ForceEnableEmoticonPlusPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/emoticon/ForceEnableEmoticonPlusPatch.kt
@@ -1,8 +1,13 @@
 package app.revanced.patches.kakaotalk.emoticon
 
 import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
+import app.morphe.patcher.extensions.InstructionExtensions.instructions
 import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.util.getReference
+import app.morphe.util.returnEarly
 import app.revanced.patches.kakaotalk.shared.Constants.COMPATIBILITY_KAKAO
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 @Suppress("unused")
 val forceEnableEmoticonPlusPatch = bytecodePatch(
@@ -16,5 +21,18 @@ val forceEnableEmoticonPlusPatch = bytecodePatch(
             1,
             "const/4 p1, 0x1"
         )
+
+        UserPreferenceIsActiveEmoticonPlusFingerprint.method.returnEarly(true)
+
+        val caller = EmoticonPlusManagerIsActiveFingerprint.method.instructions
+            .first { it.opcode == Opcode.INVOKE_VIRTUAL }
+            .getReference<MethodReference>()
+        if (caller!!.definingClass != "Lcom/kakao/talk/emoticon/itemstore/plus/EmoticonPlusMeResult;") {
+            error("Force enable emoticon plus feature is not supported")
+        }
+
+        EmoticonPlusMeResultConstructorFingerprint.classDef.methods
+            .first { it.name == caller.name }
+            .returnEarly(true)
     }
 }
