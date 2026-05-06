a6a8a27878b176b008db4a75dc9800d8d1712cd2
naijun0403 <naijun0403@gmail.com>
2026-05-06 14:13:13 +0900

    fix(kakaotalk): restore force emoticon plus activation
    Fixes #40

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/emoticon/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/emoticon/Fingerprints.kt
@@ -18,3 +18,39 @@ internal object EmoticonPlusMeResultConstructorFingerprint : Fingerprint(
         Opcode.RETURN_VOID
     )
 )
+
+internal object UserPreferenceIsActiveEmoticonPlusFingerprint : Fingerprint(
+    accessFlags = listOf(AccessFlags.PUBLIC, AccessFlags.STATIC, AccessFlags.FINAL),
+    returnType = "Z",
+    strings = listOf("<this>"),
+    filters = OpcodesFilter.opcodesToFilters(
+        Opcode.CONST_STRING,
+        Opcode.INVOKE_STATIC,
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT,
+        Opcode.RETURN,
+    ),
+    custom = { _, classDef -> classDef.sourceFile == "UserPreferencesExt.kt" }
+)
+
+internal object EmoticonPlusManagerIsActiveFingerprint : Fingerprint(
+    definingClass = "Lcom/kakao/talk/emoticon/itemstore/plus",
+    accessFlags = listOf(AccessFlags.PUBLIC, AccessFlags.FINAL),
+    returnType = "Z",
+    filters = OpcodesFilter.opcodesToFilters(
+        Opcode.SGET_OBJECT,
+        Opcode.CONST_4,
+        Opcode.IF_EQZ,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT,
+        Opcode.CONST_4,
+        Opcode.IF_NE,
+        Opcode.RETURN,
+        Opcode.RETURN,
+    ),
+    custom = { _, classDef -> classDef.sourceFile == "EmoticonPlusManager.kt" }
+)
