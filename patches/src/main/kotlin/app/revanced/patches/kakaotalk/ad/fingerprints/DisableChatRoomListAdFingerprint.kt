d891bb2222ebeb8242b9e225c15046b207c5323d
AvocadoCupcake <qxw9703@outlook.com>
2026-08-20 23:07:10 -0700

    feat(kakaotalk): disable global-region list ads

--- patches/src/main/kotlin/app/revanced/patches/kakaotalk/ad/fingerprints/DisableChatRoomListAdFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/kakaotalk/ad/fingerprints/DisableChatRoomListAdFingerprint.kt
@@ -26,3 +26,18 @@ internal object ChatListAdHelperEnabledFingerprint : Fingerprint(
     ),
     custom = { _, classDef -> classDef.sourceFile == "ChatListAdHelper.kt" }
 )
+
+internal object ChatListGlobalAdEnabledFingerprint : Fingerprint(
+    accessFlags = listOf(AccessFlags.PUBLIC, AccessFlags.FINAL),
+    parameters = listOf(
+        "Landroid/content/Context;",
+        "Z",
+        "Lkotlin/coroutines/Continuation;",
+    ),
+    returnType = "Ljava/lang/Object;",
+    filters = listOf(
+        methodCall("Landroid/content/res/Resources;->getConfiguration()Landroid/content/res/Configuration;"),
+        fieldAccess("Landroid/content/res/Configuration;->orientation:I"),
+    ),
+    custom = { _, classDef -> classDef.sourceFile == "ChatListAdHelper.kt" }
+)
