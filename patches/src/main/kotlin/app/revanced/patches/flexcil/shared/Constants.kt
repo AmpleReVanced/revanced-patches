9ac4227df8df2f47b61f105b032701697521037e
naijun0403 <naijun0403@gmail.com>
2026-08-31 23:55:03 +0900

    feat(flexcil): support version `1.5.0.9`

--- patches/src/main/kotlin/app/revanced/patches/flexcil/shared/Constants.kt
+++ patches/src/main/kotlin/app/revanced/patches/flexcil/shared/Constants.kt
@@ -12,6 +12,9 @@ object Constants {
         appIconColor = 0x0096FA,
         targets = listOf(
             AppTarget(
+                version = "1.5.0.9"
+            ),
+            AppTarget(
                 version = "1.5.0.8"
             ),
             AppTarget(
